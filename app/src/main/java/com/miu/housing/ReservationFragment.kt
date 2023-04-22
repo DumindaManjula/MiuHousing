d694a4b44af5ebd181762409286f5fa7998121cc
Duminda <basnayaka.duminda@gmail.com>
2023-04-22 17:05:29 -0500

    added report damage page

--- app/src/main/java/com/miu/housing/ReservationFragment.kt
+++ app/src/main/java/com/miu/housing/ReservationFragment.kt
@@ -1,10 +1,12 @@
 package com.miu.housing
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.miu.housing.databinding.FragmentReservationBinding
 
 class ReservationFragment : Fragment() {
@@ -15,6 +17,13 @@ class ReservationFragment : Fragment() {
     ): View? {
         var view = inflater.inflate(R.layout.fragment_reservation, container, false)
         binding = FragmentReservationBinding.bind(view)
+        binding.imageButton2.setOnClickListener {
+
+            Toast.makeText(context, "to Report a Damage", Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, ReportDamageActivity::class.java)
+            intent.putExtra("message","user info") // Here message is a key to retrieve the input text in the second activity
+            startActivity(intent)
+        }
         return binding.root
     }
 }
