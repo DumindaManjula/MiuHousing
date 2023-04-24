6f41e15bdfe1516809d6feca6bb4531a5773645b
Duminda <basnayaka.duminda@gmail.com>
2023-04-24 07:34:19 -0500

    pusing code changes for report damage and request letter

--- app/src/main/java/com/miu/housing/ReportDamageActivity.kt
+++ app/src/main/java/com/miu/housing/ReportDamageActivity.kt
@@ -1,13 +1,20 @@
 package com.miu.housing
 
 import android.R
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.view.menu.MenuBuilder
 import com.miu.housing.databinding.ActivityMainBinding
 import com.miu.housing.databinding.ActivityReportDamageBinding
 import com.miu.housing.db.User
@@ -27,6 +34,25 @@ class ReportDamageActivity : AppCompatActivity() {
         val intent = intent
         val tmp = intent.getSerializableExtra("userInfo")
         user = tmp as User
+
+
+        //        var fullname = "${user?.firstName} ${user?.lastName}"
+        val actionBar = supportActionBar
+        actionBar?.setDisplayShowTitleEnabled(false)
+        actionBar?.setDisplayShowCustomEnabled(true)
+        actionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        var params = ActionBar.LayoutParams(
+            ActionBar.LayoutParams.MATCH_PARENT,
+            ActionBar.LayoutParams.MATCH_PARENT,
+            Gravity.RIGHT
+        )
+        var viewActionBar = layoutInflater.inflate(com.miu.housing.R.layout.general_top_header,null)
+        actionBar?.setCustomView(viewActionBar, params)
+//        var tview = viewActionBar.findViewById<TextView>(R.id.loginname)
+//        tview.setText("Welcome $fullname")
+        var appName = getString(com.miu.housing.R.string.app_name)
+        var aview = viewActionBar.findViewById<TextView>(com.miu.housing.R.id.apptitle)
+        aview.setText(appName)
 
         val adapter = ArrayAdapter<String>(this, R.layout.simple_spinner_item, test)
         binding.actv.setAdapter(adapter)
@@ -75,6 +101,30 @@ class ReportDamageActivity : AppCompatActivity() {
         val intent = Intent(this, HousingActivity::class.java)
         intent.putExtra("user", user)
         startActivity(intent)
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if(menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
+        menuInflater.inflate(com.miu.housing.R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            com.miu.housing.R.id.gmail -> {
+                Toast.makeText(this, item.title.toString(), Toast.LENGTH_SHORT).show()
+            }
+            com.miu.housing.R.id.logout -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                //Toast.makeText(this, "Test test", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 }
--- app/src/main/java/com/miu/housing/RequestingLetterActivity.kt
+++ app/src/main/java/com/miu/housing/RequestingLetterActivity.kt
@@ -1,12 +1,19 @@
 package com.miu.housing
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.text.Html
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuBuilder
 import com.miu.housing.databinding.ActivityRequestingLetterBinding
 import com.miu.housing.db.User
 
@@ -23,6 +30,48 @@ class RequestingLetterActivity : AppCompatActivity() {
         val intent = intent
         val tmp = intent.getSerializableExtra("userInfo")
         user = tmp as User
+
+//        var fullname = "${user?.firstName} ${user?.lastName}"
+        val actionBar = supportActionBar
+        actionBar?.setDisplayShowTitleEnabled(false)
+        actionBar?.setDisplayShowCustomEnabled(true)
+        actionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        var params = ActionBar.LayoutParams(
+            ActionBar.LayoutParams.MATCH_PARENT,
+            ActionBar.LayoutParams.MATCH_PARENT,
+            Gravity.RIGHT
+        )
+        var viewActionBar = layoutInflater.inflate(R.layout.general_top_header,null)
+        actionBar?.setCustomView(viewActionBar, params)
+//        var tview = viewActionBar.findViewById<TextView>(R.id.loginname)
+//        tview.setText("Welcome $fullname")
+        var appName = getString(R.string.app_name)
+        var aview = viewActionBar.findViewById<TextView>(R.id.apptitle)
+        aview.setText(appName)
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if(menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.gmail -> {
+                Toast.makeText(this, item.title.toString(), Toast.LENGTH_SHORT).show()
+            }
+            R.id.logout -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                //Toast.makeText(this, "Test test", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     fun requestLetter(view: View){
