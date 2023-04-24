758dabd409399103e0f7d721d0312098f12f923f
aven <aven@Ubuntu.local>
2023-04-24 11:16:43 -0500

    add complaint activity

---
+++ app/src/main/java/com/miu/housing/ComplaintActivity.kt
@@ -0,0 +1,11 @@
+package com.miu.housing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComplaintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complaint)
+    }
+}
