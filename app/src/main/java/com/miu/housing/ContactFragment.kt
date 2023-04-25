c88bc891204c32a5690a4a87ce7b0b8394175347
Duminda <basnayaka.duminda@gmail.com>
2023-04-25 11:06:25 -0500

    pushing code changes for contact us

--- app/src/main/java/com/miu/housing/ContactFragment.kt
+++ app/src/main/java/com/miu/housing/ContactFragment.kt
@@ -1,10 +1,14 @@
 package com.miu.housing
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.miu.housing.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
@@ -16,5 +20,18 @@ class ContactFragment : Fragment() {
         var view = inflater.inflate(R.layout.fragment_contact, container,false)
         binding = FragmentContactBinding.bind(view)
         return binding.root
+
+        binding.housingEmail1.setOnClickListener{
+            Toast.makeText(context,"MiuHousing will contact you soon ", Toast.LENGTH_LONG).show()
+        }
+
+        binding.housingEmail2.setOnClickListener{
+            Toast.makeText(context,"MiuHousing will contact you soon ", Toast.LENGTH_LONG).show()
+        }
+
+        binding.phoneNumber.setOnClickListener{
+            Toast.makeText(context,"MiuHousing will contact you soon ", Toast.LENGTH_LONG).show()
+        }
     }
+
 }
