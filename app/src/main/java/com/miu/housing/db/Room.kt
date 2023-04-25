f2b92e367be086ad08beb4f629b7fb1d547a4c18
Duminda <basnayaka.duminda@gmail.com>
2023-04-25 14:03:28 -0500

    added new column in Room table in database

--- app/src/main/java/com/miu/housing/db/Room.kt
+++ app/src/main/java/com/miu/housing/db/Room.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 import java.io.Serializable
 
 @Entity
-data class Room(var roomNumber:String?, var roomType:String?, var price:Int? , var size:Double?, var status:String?, var image: Bitmap?):
+data class Room(var roomNumber:String?, var roomType:String?, var price:Int? , var size:Double?, var status:String?, var image: Bitmap?, var buildingId:Int? ):
     Serializable {
     @PrimaryKey(autoGenerate = true)
     var id:Int = 0
