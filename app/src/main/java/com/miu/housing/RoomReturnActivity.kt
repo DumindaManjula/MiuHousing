d234979674ee32c8baea482f8bb5e6b105346d5f
Bisrat Betre <bisratbetre5@gmail.com>
2023-04-26 18:56:45 -0500

    modify return

--- app/src/main/java/com/miu/housing/RoomReturnActivity.kt
+++ app/src/main/java/com/miu/housing/RoomReturnActivity.kt
@@ -3,48 +3,56 @@ package com.miu.housing
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
 import com.miu.housing.databinding.RoomReturnBinding
 import com.miu.housing.db.Booking
 import com.miu.housing.db.MiuHousingDatabase
 import com.miu.housing.db.Room
 import com.miu.housing.db.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class RoomReturnActivity : BaseActivity() {
     private lateinit var binding : RoomReturnBinding
      var room: Room? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding= RoomReturnBinding.inflate(layoutInflater)
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        var roomImage = binding.roomImages
-        var buildingName = binding.buildingName
-        var roomNumber = binding.roomNumber
-        var roomType = binding.roomType
+//        var roomImage = binding.roomImages
+//        var buildingName = binding.buildingName
+//        var roomNumber = binding.roomNumber
+//        var roomType = binding.roomType
 
         val intent = intent
         val tmp = intent.getSerializableExtra("userInfo")
         var user = tmp as User
 
-        launch {
+        lifecycleScope.launch {
             applicationContext?.let {
+                withContext(Dispatchers.Main){
                 var bookingReturn = MiuHousingDatabase(it).getBookingDao().getBooking(user.id)
                 room = MiuHousingDatabase(it).getRoomDao().getRoom(bookingReturn.room_number)
-                room?.let { it ->
-                    roomImage.setImageResource(it.image)
-                    roomNumber.text = it.Room_no.toString()
-                    roomType.text = it.Room_type
-                    buildingName.text= it.buildingName
+
+                    Log.i("MY_MIU_TAG", room.toString())
                 }
-
-
-
-
-
+                withContext(Dispatchers.Main){
+                    room?.let { it ->
+                        binding.roomImages.setImageResource(it.image)
+                        binding.roomNumber.text = it.Room_no.toString()
+                        binding.roomType.text = it.Room_type.toString()
+                        binding.buildingName.text= it.buildingName
+                    }
+                }
             }}
+
 
         val button = binding.button
 
