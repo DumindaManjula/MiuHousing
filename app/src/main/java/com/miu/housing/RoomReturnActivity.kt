e1519f8624dc08e8f57e76841e4f11b99a060755
Bisrat Betre <bisratbetre5@gmail.com>
2023-04-26 18:16:52 -0500

    pushing reservation code chnages to room-reservation br

---
+++ app/src/main/java/com/miu/housing/RoomReturnActivity.kt
@@ -0,0 +1,10 @@
+package com.miu.housing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoomReturnActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
