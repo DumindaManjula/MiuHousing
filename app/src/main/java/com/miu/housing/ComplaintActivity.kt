706839969dc5ea8ff9513c7cc1910d2fbcc5cb46
Duminda <basnayaka.duminda@gmail.com>
2023-04-26 10:09:16 -0500

    pushing code changes made for complain module

--- app/src/main/java/com/miu/housing/ComplaintActivity.kt
+++ app/src/main/java/com/miu/housing/ComplaintActivity.kt
@@ -1,10 +1,15 @@
 package com.miu.housing
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.view.menu.MenuBuilder
 import com.miu.housing.databinding.ActivityComplaintBinding
 import com.miu.housing.databinding.ActivityReportDamageBinding
 import com.miu.housing.db.User
@@ -21,8 +26,16 @@ class ComplaintActivity : AppCompatActivity() {
         val intent = intent
         val tmp = intent.getSerializableExtra("userInfo")
         user = tmp as User
+
+        var rightText = "${user?.firstName} ${user?.lastName}"
+        var leftText = getString(com.miu.housing.R.string.app_name) + " - Make Complain"
+
+        val actionBar: ActionBar? = supportActionBar
+        if(actionBar != null) {
+            showCustomActionBar(actionBar, layoutInflater, leftText, rightText)
+        }
     }
-    fun submitDamage(view: View){
+    fun submitComplain(view: View){
         val dmgReason = binding.editTextReason.text
 
         if (dmgReason.isBlank()) {
@@ -31,18 +44,42 @@ class ComplaintActivity : AppCompatActivity() {
             return
         }
 
-        Toast.makeText(this,"MiuHousing will contact you soon ", Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"Compliant has been sent.MiuHousing will contact you soon ", Toast.LENGTH_LONG).show()
 
         val intent = Intent(this, HousingActivity::class.java)
         intent.putExtra("user", user)
         startActivity(intent)
 
     }
-    fun cancelDamage(view: View){
+    fun cancelComplain(view: View){
 
         val intent = Intent(this, HousingActivity::class.java)
         intent.putExtra("user", user)
         startActivity(intent)
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if(menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
+        menuInflater.inflate(com.miu.housing.R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when(item.itemId) {
+//            com.miu.housing.R.id.gmail -> {
+//                Toast.makeText(this, item.title.toString(), Toast.LENGTH_SHORT).show()
+//            }
+//            com.miu.housing.R.id.logout -> {
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+//                //Toast.makeText(this, "Test test", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 }
--- app/src/main/java/com/miu/housing/ReportDamageActivity.kt
+++ app/src/main/java/com/miu/housing/ReportDamageActivity.kt
@@ -117,14 +117,14 @@ class ReportDamageActivity : BaseActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
-            com.miu.housing.R.id.gmail -> {
-                Toast.makeText(this, item.title.toString(), Toast.LENGTH_SHORT).show()
-            }
-            com.miu.housing.R.id.logout -> {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                //Toast.makeText(this, "Test test", Toast.LENGTH_SHORT).show()
-            }
+//            com.miu.housing.R.id.gmail -> {
+//                Toast.makeText(this, item.title.toString(), Toast.LENGTH_SHORT).show()
+//            }
+//            com.miu.housing.R.id.logout -> {
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+//                //Toast.makeText(this, "Test test", Toast.LENGTH_SHORT).show()
+//            }
         }
 
         return super.onOptionsItemSelected(item)
